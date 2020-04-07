9fb4436e924d61c7442ece01c0406afc1963e6d5
Henk Kin <microsoft@henkkin.nl>
2020-04-07 13:36:25 +0200

    Added IConvertible implementation to Identifier Set version to 1.0.7

--- Identifiers/Identifier.cs
+++ Identifiers/Identifier.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 namespace Identifiers
 {
-    public struct Identifier : IComparable<Identifier>, IEquatable<Identifier>
+    public struct Identifier : IComparable<Identifier>, IEquatable<Identifier>, IConvertible
     {
         private readonly object _value;
 
@@ -79,6 +79,91 @@ namespace Identifiers
         public override string ToString()
         {
             return _value != null ? _value.ToString() : string.Empty;
+        }
+
+        public TypeCode GetTypeCode()
+        {
+            return TypeCode.Object;
+        }
+
+        public bool ToBoolean(IFormatProvider provider)
+        {
+            return Convert.ToBoolean(_value, provider);
+        }
+
+        public byte ToByte(IFormatProvider provider)
+        {
+            return Convert.ToByte(_value, provider);
+        }
+
+        public char ToChar(IFormatProvider provider)
+        {
+            return Convert.ToChar(_value, provider);
+        }
+
+        public DateTime ToDateTime(IFormatProvider provider)
+        {
+            return Convert.ToDateTime(_value, provider);
+        }
+
+        public decimal ToDecimal(IFormatProvider provider)
+        {
+            return Convert.ToDecimal(_value, provider);
+        }
+
+        public double ToDouble(IFormatProvider provider)
+        {
+            return Convert.ToDouble(_value, provider);
+        }
+
+        public short ToInt16(IFormatProvider provider)
+        {
+            return Convert.ToInt16(_value, provider);
+        }
+
+        public int ToInt32(IFormatProvider provider)
+        {
+            return Convert.ToInt32(_value, provider);
+        }
+
+        public long ToInt64(IFormatProvider provider)
+        {
+            return Convert.ToInt64(_value, provider);
+        }
+
+        public sbyte ToSByte(IFormatProvider provider)
+        {
+            return Convert.ToSByte(_value, provider);
+        }
+
+        public float ToSingle(IFormatProvider provider)
+        {
+            return Convert.ToSingle(_value, provider);
+        }
+
+        public string ToString(IFormatProvider provider)
+        {
+            return _value == null ? null : Convert.ToString(_value, provider);
+        }
+
+        public object ToType(Type conversionType, IFormatProvider provider)
+        {
+            return Convert.ChangeType(_value, conversionType, provider);
+        }
+
+        public ushort ToUInt16(IFormatProvider provider)
+        {
+            return Convert.ToUInt16(_value, provider);
+        }
+
+        public uint ToUInt32(IFormatProvider provider)
+        {
+            return Convert.ToUInt32(_value, provider);
+        }
+
+        public ulong ToUInt64(IFormatProvider provider)
+        {
+            return Convert.ToUInt64(_value, provider);
         }
     }
 }
